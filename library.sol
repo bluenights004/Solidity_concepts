97b225e6a038dbf6fab76c0bf006fce734e00515
OceanSky <bluenights004@gmail.com>
2023-01-12 12:15:41 +0800

    to demo on how to library in smart contract

---
+++ library.sol
@@ -0,0 +1,27 @@
+/ SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.17;
+
+library Lib {
+    function multiply (uint x, uint y) pure internal returns (uint) {
+        return x * y;
+    }
+}
+
+contract M {
+
+    using Lib for uint;
+
+    uint z;
+
+    function set(uint _z) public {
+        z = _z; 
+    }
+
+    function addMultiply (uint x, uint y) view external returns (uint) {
+        return z + x.multiply(y);
+    }
+}
+
+
+
